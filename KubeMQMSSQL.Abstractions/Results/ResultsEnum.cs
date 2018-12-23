4e2d5608c16bf8f971027d1505969bd1a5efca1e
eitam ring <eitam.ring@kubemq.io>
2018-12-23 14:11:10 +0000

    Added support in DataReader types: 1)DataReader 2)Reader with Slice 3)Reader with multiTable
    Added Stream Parameter Class.

--- KubeMQMSSQL.Abstractions/Results/ResultsEnum.cs
+++ KubeMQMSSQL.Abstractions/Results/ResultsEnum.cs
@@ -4,19 +4,28 @@ using System.Text;
 
 namespace KubeMQMSSQL.Abstractions.Results
 {
+    /// <summary>
+    /// ResultEnum , represent if there was an error while preforming the request.
+    /// </summary>
     [Serializable]
     public enum ResultsEnum
     {
         Done,
-        Error=999,
+        Error = 999,
     }
 
+    /// <summary>
+    /// Represent the type of action to preform with the KubeMQMSSQL worker.
+    /// </summary>
     [Serializable]
     public enum ProceduresType
     {
         Adapter,
         NonQuery,
         Scalar,
-        NotSupported
+        ExecuteDataReader,
+        ExecuteDataReaderWithSlice,
+        ExecuteDataReaderWithMultiTable,
+        NotSupported = 99999,
     }
 }
---
+++ KubeMQMSSQL.Abstractions/StreamParameters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KubeMSSQL.Abstractions
+{
+    /// <summary>
+    /// A class that contain custom parameters of stream requests.
+    /// </summary>
+    [Serializable]
+    public class StreamParameters
+    {
+        /// <summary>
+        /// Represent the amount of delay between events sending.(milliseconds).
+        /// </summary>
+        public int DelayBetweenEvents { get; set; }
+        /// <summary>
+        /// Represent the name of the channel to return reader events to.
+        /// </summary>
+        public string ReaderReturnChannel { get; set; }
+        /// <summary>
+        /// Represent the threshold amount of data rows in one Event to receive from KubeMQ MSSQLConnector.
+        /// If left empty setting to default of 1.
+        /// </summary>
+        public int SliceThreshold { get; set; }
+        /// <summary>
+        /// Initialize a new instance of KubeMSSQL.Abstractions.StreamParameters.
+        /// </summary>
+        public StreamParameters()
+        {
+            SliceThreshold = 1;
+        }
+    }
+}
